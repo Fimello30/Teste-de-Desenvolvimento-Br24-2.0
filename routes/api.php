15f2d29fcd729435fe6befdf33c0fe2eff3f7656
Fimello30 <Fi.mello@hotmail.com>
2020-06-21 19:10:46 -0400

    correção deals

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/deals',['uses'=>'DealsController@index']);
+Route::get('/deals',['uses'=>'DealsController@index']);
